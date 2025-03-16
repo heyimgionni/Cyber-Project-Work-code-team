ceebca3fdd8f72b50a722f77d52bbf116c47308d
Xhoni Sheqi <heyimgionni@gmail.com>
2025-03-16 10:49:37 +0100

    Aggiunto calcolo entropia e visual box per entropia , sistemati alcuni problemi , Da risolvere quando genera 8 char di password a volte missa il numero nonostante sia selezionato ogni n+2 volte

--- scripts/passwordChecker.js
+++ scripts/passwordChecker.js
@@ -2,6 +2,8 @@ import costrains from "./utils/constrains";
 import levels from "./utils/level";
 import fetchData from "./utils/fetchHYBP";
 import handleOverlayAnimation from "./utils/overlayMsg";
+import handleTime from "./utils/handleTIme";
+import gsap from "gsap";
 
 document.addEventListener("DOMContentLoaded", () => {
   const seePasswordBtn = document.querySelector(".input__zone i");
@@ -13,6 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const checkBtn = document.querySelector(".btn-check");
   const overlay = document.querySelector(".overlay");
   const overlayText = document.querySelector(".overlay p");
+  const entropyBox = document.querySelector(".entropy__box");
 
   const handleVisibilityPassword = () => {
     const isVisible = seePasswordBtn.classList.contains("fa-eye");
@@ -35,40 +38,38 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   };
 
-  const calculateSetChar = (password) => {
-    let hasLower = false;
-    let hasUpper = false;
-    let hasDigits = false;
-    let hasSpecial = false;
-    let charForEntropy = 0;
-    const reLower = /[a-z]/;
-    const reUpper = /[A-Z]/;
-    const reDigits = /\d/;
-    const reSpecial = /[!@#$%^&*(),.?":{}|<>]/;
-
-    for (let letter of password) {
-      if (reLower.test(letter)) hasLower = true;
-      if (reUpper.test(letter)) hasUpper = true;
-      if (reDigits.test(letter)) hasDigits = true;
-      if (reSpecial.test(letter)) hasSpecial = true;
-    }
-    hasLower ? (charForEntropy += 26) : null;
-    hasUpper ? (charForEntropy += 26) : null;
-    hasDigits ? (charForEntropy += 10) : null;
-    hasSpecial ? (charForEntropy += 32) : null;
-    return charForEntropy || 1;
-  };
-
   const handleCalculationEntropy = () => {
     const password = input.value;
     const lenPass = password.length;
-    const set = calculateSetChar(password);
-    let entropy = Math.floor(lenPass * Math.log2(set));
+    let charSet = 0;
+
+    if (/[a-z]/.test(password)) charSet += 26; // Lettere minuscole
+    if (/[A-Z]/.test(password)) charSet += 26; // Lettere maiuscole
+    if (/[0-9]/.test(password)) charSet += 10; // Numeri
+    if (/[^a-zA-Z0-9]/.test(password)) charSet += 32; // Simbolo standard ASCII
+
+    if (charSet === 0) return 0;
+    let entropy = Math.floor(lenPass * Math.log2(charSet));
     return entropy;
+  };
+
+  const handleTimeToCrack = (entropy, attemps) => {
+    /*
+      come funziona l'entropia 
+      totaleCombinazioni  = 2 alla entrpia n
+      un attaccante in forza bruta ci impiega una media di totale / 2
+      e per stimare il tempo si calcola mediaTentativi / tentativiPerSecondi
+    */
+    let totalCombination = Math.pow(2, entropy);
+    let avgAttemps = totalCombination / 2;
+    let time = avgAttemps / attemps;
+
+    return handleTime(time);
   };
 
   const handleSecurityBar = () => {
     const entropy = handleCalculationEntropy();
+    // .find(l (of levels) => cb of entropy minore uguale a l.max)
     const level = levels.find((l) => entropy <= l.max);
     securityPar.innerHTML = level.text;
     securityBar.style.width = level.width;
@@ -81,7 +82,34 @@ document.addEventListener("DOMContentLoaded", () => {
     handlePasswordValidation();
     handleSecurityBar();
   });
-  entropyBtn.addEventListener("click", handleCalculationEntropy);
+  entropyBtn.addEventListener("click", () => {
+    const entropy = handleCalculationEntropy();
+    let combination = {
+      "PC Normale": handleTimeToCrack(entropy, 10 ** 6),
+      "GPU Avanzata ( Aka Per Ricchi ) ": handleTimeToCrack(entropy, 10 ** 9),
+      SuperComputer: handleTimeToCrack(entropy, 10 ** 12),
+    };
+
+    let messageEntropy = `
+      <div class="entropy__container">
+        <p><strong>Entropy: ${entropy}</strong></p>
+        <ul>
+          <li>PC NORMALE : ${combination["PC Normale"]}</li>
+          <li>GPU AVANZATA : ${combination["GPU Avanzata ( Aka Per Ricchi ) "]}</li>
+          <li>SUPERCOMPUTER : ${combination["SuperComputer"]}</li>
+        </ul>
+        <img class="close" src="./assets/x.svg" alt="close button" />
+      </div>
+    `;
+    gsap.to(entropyBox, { display: "block", ease: "power1.inOut" });
+    entropyBox.innerHTML = messageEntropy;
+    const close = document.querySelector(".entropy__box .close");
+    if (close) {
+      close.addEventListener("click", () => {
+        gsap.to(entropyBox, { display: "none" });
+      });
+    }
+  });
   checkBtn.addEventListener("click", () => {
     const password = input.value;
     if (password) handleOverlayAnimation(overlay);
